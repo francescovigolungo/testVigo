a8aef422dc40263411470ff4e16db9296c8db699
francesco <francesco.vigolungo@wi-tek.it>
2017-09-14 12:07:45 +0200

    1 - prima commit

--- test.ts
+++ test.ts
@@ -4,9 +4,16 @@ export class vigo{
     b: number;
     c: number;
 
+    d: number;
+    e: number;
+    f: number;
+
     constructor(){
         this.a = 1;
         this.b = 2;
         this.b = 3;
+        this.d = 4;
+        this.e = 5;
+        this.f = 6;
     }
 }
