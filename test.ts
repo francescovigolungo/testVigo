76335eb96d30637494a4fab43e0bba9cb3e35c76
francesco <francesco.vigolungo@wi-tek.it>
2017-09-14 12:08:54 +0200

    3 - terza commit

--- test.ts
+++ test.ts
@@ -8,6 +8,10 @@ export class vigo{
     e: number;
     f: number;
 
+    h: number;
+    i: number;
+    l: number;
+
     constructor(){
         this.a = 1;
         this.b = 2;
@@ -15,5 +19,8 @@ export class vigo{
         this.d = 4;
         this.e = 5;
         this.f = 6;
+        this.h = 7;
+        this.i = 8;
+        this.l = 9;
     }
 }
